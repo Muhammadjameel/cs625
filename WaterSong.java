53fc4d8a3cf0393a45f0d75307e5478029d265b5
Muhammadjameel <48083165+Muhammadjameel@users.noreply.github.com>
2019-03-04 03:11:01 -0800

    first commit

---
+++ WaterSong.java
@@ -0,0 +1,24 @@
+public class WaterSong {
+public static void main ( String[]args){
+int water num =9;
+string word="bottles";yy
+while(waternum>0){
+if(waternum=1)
+{
+word="bottle";
+}
+System.out.println(waternum+""word+"of water on the wall");
+System.out.println(waternum+""+word+'of water");
+Syatem.out.println("take one down");
+System.out.println("pass it around");
+waternum=waternum-1;
+if (waternum==1){
+System.out.println(waternum+""+word+"of water on the wall");
+}
+else
+{
+System.out.println("nomore bottles of water on the wall");
+    }
+   }
+  }
+ }
